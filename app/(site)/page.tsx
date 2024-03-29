7a83fcb7fe7d9f79f401987e885bc5aecfeca9cd
liwenkai <2020583117@qq.com>
2024-03-29 22:52:20 +0800

    fix: 简单添加后续文件

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Header from '@/components/header'
+import Main from '@/components/main'
 
 export default function Home() {
-	return <div>home</div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  )
 }
